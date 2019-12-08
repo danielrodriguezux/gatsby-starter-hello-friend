ab5989323d795253f76c875c2eb7489c469c7164
dannyfrommiami <danielrodriguezux@gmail.com>
2019-12-08 13:39:51 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       alt: '',
     },
     logoText: 'dannyfrommiami',
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
     postsPerPage: 5,
     showMenuItems: 2,
     menuMoreText: 'Show more',
